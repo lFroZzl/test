8a9c14e11f4364be1f6c9aaf9d8d8f010374586a
FroZz <mafiondres12@gmail.com>
2021-04-07 14:31:21 -0700

    agregado tooltip on click + css fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,38 @@ $(function () {
 
     $('[data-toggle="tooltip"]').tooltip()
 
-    
+    $(".click-me-1").click(function () {
+        $(".texto-card-1").toggle("slow", function () {
+
+        })
+    })
+
+    $(".click-me-2").click(function () {
+        $(".texto-card-2").toggle("slow", function () {
+
+        })
+    })
+
+    $(".click-me-3").click(function () {
+        $(".texto-card-3").toggle("slow", function () {
+
+        })
+    })   
 })
+
+
+/* 
+
+    //para texto invisible por defecto //
+
+    $(".card").ready(function () {
+        var par = $('.invi')
+        $(par).hide()
+
+        $('#button').click(function (e) {
+            $(par).slideToggle('slow');
+            e.preventDefault()
+        })
+    })
+
+*/
